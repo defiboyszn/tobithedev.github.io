6e6d18afeb50aa7b55fa1dbe2c38a6bd3d836571
Tobi <showgame360@gmail.com>
2022-03-02 15:36:44 -0800

    new update

---
+++ assets/sveltekit.a52f2ff4.js
@@ -0,0 +1,15 @@
+const t={title:"Sveltekit Remains The Best FullStack Framework",author:"tobithedev",tag:"coding",date:"2/25/2022",headerImg:"/sveltekit-tweet.png",id:1},e=`<h1>So SomeDays Ago I tweeted a poll on twitter listing 4 fullStack javascript framework</h1>
+<ul>
+<li>Sveltekit</li>
+<li>Remix</li>
+<li>Nuxt</li>
+<li>Next</li>
+</ul>
+<h3>The Result From the poll Was Sveltekit \u{1F3C6} \u{1F973} \u{1F973}</h3>
+<p>Incase You Don't Know SvelteKit</p>
+<h1>Sveltekit is a fullStack framework powered by sveltejs</h1>
+<p>it was created by <strong>Rich Harris</strong> Also the creator of rollupjs</p>
+<blockquote>
+<p>I'm a Big Fan Of Rich Harris and His Contents \u{1F370}</p>
+</blockquote>
+`;export{t as attributes,e as html};
