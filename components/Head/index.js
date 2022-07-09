ad4d8ba968d6a8ee2a063efc29c86c25c5a365c3
Tobi <showgame360@gmail.com>
2022-07-08 22:37:48 -0700

    feat: new update

--- components/Head/index.js
+++ components/Head/index.js
@@ -5,25 +5,32 @@ export default function DomHead({ pageName = "Home Page" }) {
 
     return (
         <Head>
-            <title>Baaymax - {pageName} </title>
-            {/* meta tags begins */}
-            {/* Primary Meta Tags */}
-            <meta name="title" content="BaayMax — Generate a unique developer portfolio with just one click" />
-            <meta name="description" content="Generating of portfolio just got easier. Using BaayMax and a single command, you're on set." />
-            {/* Open Graph / Facebook */}
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://github.com/benrobo/baaymax-cli/" />
-            <meta property="og:title" content="BaayMax — Generate a unique developer portfolio with just one click" />
-            <meta property="og:description" content="Generating of portfolio just got easier. Using BaayMax and a single command, you're on set." />
-            <meta property="og:image" content="https://github.com/Benrobo/baaymax-cli/raw/master/readmeImg/cli.png?raw=true" />
+            <title>Tobithealpha - {pageName} </title>
+            <meta charset="UTF-8" />
+    <link rel="icon" type="image/ico" href="favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    {/* <!-- Primary Meta Tags --> */}
+    <meta
+      name="keywords"
+      content="tobithealpha,tobithedev,emekathealpha,uche emeka tobi,uche chukwuemeka oluwatobi,texxalabs, texxalabs ceo, texxalabs founder,dativejs,creator of dativejs,founder of frixxapp"
+    />
+    <title>TobitheAlpha</title>
+    <meta name="title" content="TobitheAlpha"/>
+    <meta name="description" content="TobitheAlpha is the Founder of Texxalabs" />
+    <meta name="google-site-verification" content="ByREhs1KkqW2wE-NBlvHR3OEkMU5lbiSSYNmAZ_6qJw" />
+    {/* <!-- Open Graph / Facebook --> */}
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://tobithedev.github.io/" />
+    <meta property="og:title" content="TobitheAlpha" />
+    <meta property="og:description" content="TobitheAlpha is the Founder of Texxalabs Technology" />
+    <meta property="og:image" content="tobi.png" />
 
-            {/* <!-- Twitter --> */}
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://metatags.io/" />
-            <meta property="twitter:title" content="BaayMax — Generate a unique developer portfolio with just one click" />
-            <meta property="twitter:description" content="Generating of portfolio just got easier. Using BaayMax and a single command, you're on set." />
-            <meta property="twitter:image" content="https://github.com/Benrobo/baaymax-cli/raw/master/readmeImg/cli.png?raw=true" />
-
+    {/* <!-- Twitter --> */}
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://tobithedev.github.io/" />
+    <meta property="twitter:title" content="TobitheAlpha" />
+    <meta property="twitter:description" content="TobitheAlpha is the Founder of Texxalabs Technology" />
+    <meta property="twitter:image" content="tobi.png" />
             {/* meta tags end */}
             <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
             <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
