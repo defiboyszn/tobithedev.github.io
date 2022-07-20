f890e39737509b2e42cc884cc5ea8d2504d8f36d
TobitheAlpha <showgame360@gmail.com>
2022-07-20 09:15:31 -0700

    yikesssss

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@
 
 // EMAILJS Config Data
 
-export const EMAILJS_TEMPLATE_ID = ""
+export const EMAILJS_TEMPLATE_ID = "template_x2k7u3b"
 
-export const EMAILJS_SERVICE_ID = ""
+export const EMAILJS_SERVICE_ID = "service_2yo4ulc"
 
-export const EMAILJS_PUBLIC_KEY = ""
+export const EMAILJS_PUBLIC_KEY = "7HsrfuGwS_5i7_ym1"
